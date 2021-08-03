a0ad10b02729ecd085a61c0b43bb90347798f10b
曹晓鹏 <caoxiaopeng@xdf.cn>
2021-08-03 18:36:07 +0800

    fix(x-main): 添加子应用mciro配置
    affects: x-main

--- packages/x-main/src/micro/app.ts
+++ packages/x-main/src/micro/app.ts
@@ -10,6 +10,12 @@ const apps = [
     entry: '//10.201.140.75:8082',
     container: '#micro_wrapper',
     activeRule: '/channelPage'
+  },
+  {
+    name: 'x-info',
+    entry: '//10.201.140.75:8083',
+    container: '#micro_wrapper',
+    activeRule: '/infoPage'
   }
 ]
 export default apps
